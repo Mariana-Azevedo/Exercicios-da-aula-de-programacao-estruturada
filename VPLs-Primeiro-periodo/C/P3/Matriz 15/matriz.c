d5bd7a4bcd9a3e6406a61bf06ef3b4c9f680b02d
Mariana-Azevedo <96557985+Mariana-Azevedo@users.noreply.github.com>
2022-09-30 00:31:13 -0300

    .

--- VPLs-Primeiro-periodo/C/P3/Matriz 15/matriz.c
+++ VPLs-Primeiro-periodo/C/P3/Matriz 15/matriz.c
@@ -1,26 +1,54 @@
-//#include "biblioteca.h"
+#include <stdio.h>
 
-int verifica_linhas(int matriz[][3], int linhas, int colunas)
-{
-    return 0;
-}
+int main()
+    {
+        int matriz[3][3], linhas, colunas, i, j;
+        for(i=0; i<3; i++)
+            {   
+                for(j=0;j<3; j++)
+                    {
+                        scanf("%d", &matriz[i][j]);
+                        if( ( matriz<1 || matriz>9 ) )
+                        {
+                            printf("digite outro valor");
+                            matriz[i][j]--;
+                        }
+                    }
+            }
+            printf("\n");
+             for(i=0; i<3; i++)
+            {
+                for(j=0;j<3; j++)
+                    {     
+                      printf("%d ", matriz[i][j]);
+                    }
+                      printf("\n");
+            }
+       // printf("%d", verifica_matriz(matriz[i][j], linhas, colunas));
+        return 0;
+    }
+
+/*int verifica_linhas(int matriz[][3], int linhas, int colunas)
+    {
+        return 0;
+    }
 
 int verifica_colunas(int matriz[][3], int linhas, int colunas)
-{
-    return 0;
-}
+    {
+        return 0;
+    }
 
 int verifica_diagonais(int matriz[][3], int linhas, int colunas)
-{
-    return 0;
-}
+    {
+        return 0;
+    }
 
 int verifica_numeros_unicos(int matriz[][3], int linhas, int colunas)
-{
-    return 0;
-}
+    {
+        return 0;
+    }
 
 int verifica_matriz(int matriz[][3], int linhas, int colunas)
-{
-    return 0;
-}
+    {
+        return 0;
+    }*/
---
+++ VPLs-Primeiro-periodo/Javascript/exercícios/Area do circulo/area_circulo.js
@@ -0,0 +1,4 @@
+var readlineSync = require('readline-sync');
+var raio;
+var area;
+
